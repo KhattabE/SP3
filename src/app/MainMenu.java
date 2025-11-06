a12c87513cde37c882630fe51d31322a40e72e00
khat0076 <khattab2003@live.com>
2025-11-06 20:39:42 +0100

    .

--- src/app/MainMenu.java
+++ src/app/MainMenu.java
@@ -1,5 +1,7 @@
 package app;
 
+import app.util.UIText;
+
 import java.util.ArrayList;
 
 public class MainMenu {
--- src/app/util/FileIO.java
+++ src/app/util/FileIO.java
@@ -8,10 +8,16 @@ import java.util.ArrayList;
 
 public class FileIO {
 
-    public static void saveUsers(String filepath, ArrayList<User> users){
+    public void saveUsers(String filepath, ArrayList<User> users){
 
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(filepath));
+            writer.write("name;mail;code\n");
+            for (User user : users) {
+                writer.write(user.getName() + ";" + user.getMail() + ";" + user.getCode() + "\n");
+            }
+
+            writer.close();
 
         }catch (IOException ioe){
             System.out.println("Could not write to file!");
