f611ed5cc80b80ec544d60b8977bc70830a2eee6
AmaanM3 <amaan3m@gmail.com>
2025-11-06 14:16:56 +0100

    Created the GenreManager class but not fully coded it yet

---
+++ src/GenreManager.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class GenreManager {
+
+    private ArrayList<Genre> genreList;
+
+    public GenreManager(ArrayList<Genre> genreList) {
+        this.genreList = genreList;
+    }
+
+    public void addGenre(Genre genre){
+        // Add a genre
+    }
+
+    public ArrayList<Genre> getAllGenres(){
+        return 0;
+       // Show all genres
+    }
+
+    public Genre getGenreByName(String name){
+        // Find genre by name
+    }
+
+    public void printGenres(){
+        // Prints genres
+    }
+
+}
